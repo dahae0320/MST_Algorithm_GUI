68b814d09a1ba1689e0c757643e89b3b1f3cc7dc
dahae0320 <ldh990320ldh@gmail.com>
2021-12-05 22:47:27 +0900

    원과 직선 충돌시 간선 그리지 못하게 설정

--- MST.py
+++ MST.py
@@ -1,6 +1,5 @@
-from sys import exec_prefix
-import tkinter
-import tkinter.ttk
+import tkinter, tkinter.ttk
+import tkinter.messagebox as message
 from math import *
 
 window = tkinter.Tk()
@@ -19,7 +18,7 @@ def drawingNode(event):
   y = event.y
   main_canvas.create_oval(x-13,y-13, x+13, y+13, fill="skyblue", width=0)
   main_canvas.create_text(x, y, text=str(node_number))
-  node.append([x,y])
+  node.append([x,y,node_number])
   # 그려진 노드를 출발지, 목적지 노드 콤보박스로 넣기
   start_node['value'] = [v for v in range(1, node_number+1)]
   end_node['value'] = [v for v in range(1, node_number+1)]
@@ -31,21 +30,49 @@ def drawingNode(event):
   print("node_V : ", node_V)
 
 
-def drawingEdge(event, start, end, weight):
+def drawingEdge(event, start: str, end: str, weight: str):
   s_x = node[int(start)-1][0]
   s_y = node[int(start)-1][1]
   e_x = node[int(end)-1][0]
   e_y = node[int(end)-1][1]
-  w_x, w_y = drawingWeight(s_x, s_y, e_x, e_y)
-  # 간선과 가중치 그리기
-  main_canvas.create_line(s_x, s_y, e_x, e_y, fill="black")
-  main_canvas.create_text(w_x, w_y, text=weight, fill="blue")
-  # 노드의 가중치 배열에 가중치 넣기
-  node_V[int(start)-1][int(end)-1] = weight
-  node_V[int(end)-1][int(start)-1] = weight
+  w_x, w_y = drawingWeight(int(s_x), int(s_y), int(e_x), int(e_y))
+  # 간선이 다른 원과 중복되어 그려지지 않도록 확인한다.
+  if doesCollide(node, s_x, s_y, e_x, e_y):
+    message.showwarning("간선 중복 오류", "간선이 다른 원에 겹칩니다! 다시 선택하여 주세요.")
+  else:
+    # 간선과 가중치 그리기
+    main_canvas.create_line(s_x, s_y, e_x, e_y, fill="black")
+    main_canvas.create_text(w_x, w_y, text=weight, fill="blue")
+    # 노드의 가중치 배열에 가중치 넣기
+    node_V[int(start)-1][int(end)-1] = weight
+    node_V[int(end)-1][int(start)-1] = weight
 
   print("node_V, Edge :", node_V)
 
+def doesCollide(node, x1, y1, x2, y2) -> bool:
+  # start(x1, y1), end(x2, y2)
+  # y=ax+c, ax+by+c=0, b=-1
+  a = abs(y1-y2) / abs(x1-x2)
+  b = -1
+  c = y1 - a*x1
+  x_min=0; x_max=0; y_min=0; y_max=0
+
+  if x1 > x2:
+    x_min = x2; x_max = x1
+  else:
+    x_min = x1; x_max = x2
+  if y1 > y2:
+    y_min = y2; y_max = y1
+  else:
+    y_min = y1; y_max = y2
+
+  for x, y, num in node:
+    print(x_min, x_max, y_min, y_max, x, y)
+    if x_min < x < x_max and y_min < y < y_max:
+      d = abs(a*x1 + b*y1 + c) / sqrt(a**2 + b**2)  # 점과 직선 사이의 거리
+      if d <= 13: # 반지름의 길이 : 13
+        return True
+  return False
 
 def drawingWeight(s_x, s_y, e_x, e_y):
   if s_x > e_x:
@@ -60,6 +87,20 @@ def drawingWeight(s_x, s_y, e_x, e_y):
 
   return (mid_x, mid_y)
 
+def drawingPrimNode(event):
+  for n in range(len(node)):
+    x = node[n][0]
+    y = node[n][1]
+    prim_canvas.create_oval(x-13,y-13, x+13, y+13, fill="skyblue", width=0)
+    prim_canvas.create_text(x, y, text=str(n+1))
+  
+
+def drawingKruscalNode(event):
+  for n in range(len(node)):
+    x = node[n][0]
+    y = node[n][1]
+    kruskal_canvas.create_oval(x-13,y-13, x+13, y+13, fill="skyblue", width=0)
+    kruskal_canvas.create_text(x, y, text=str(n+1))
 
 # 프레임 설정
 # frame1
@@ -120,17 +161,19 @@ labelframe.pack()
 
 mid_result=tkinter.Button(labelframe, text="중간 결과")
 mid_result.pack(side="left")
+
 final_result=tkinter.Button(labelframe, text="최종 결과")
 final_result.pack(side="right")
+final_result.bind("<Button-1>", drawingPrimNode)
+# final_result.bind("<Button-1>", drawingKruscalNode)
 
 ## frame3
 prim_canvas=tkinter.Canvas(frame3, bd=2)
 prim_canvas.place(x=0, y=0, width=450, height=400)
-# prim_canvas.bind("<Button-1>", drawingNode)
 
 ## frame4
 kruskal_canvas=tkinter.Canvas(frame4, bd=2)
 kruskal_canvas.place(x=0, y=0, width=450, height=400)
-# kruskal_canvas.bind("<Button-1>", drawingNode)
+# kruskal_canvas.bind("<Button-1>", drawingKruscalNode)
 
 window.mainloop()
